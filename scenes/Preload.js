176e099c74b3c178ccb807daef148e58774794c2
Haleynp <haleynpatel@gmail.com>
2025-03-03 16:30:41 -0500

    Add shermie asset as the player

--- scenes/Preload.js
+++ scenes/Preload.js
@@ -4,7 +4,12 @@ export default class PreloadScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image("player", "assets/shermie.png");
+        //this.load.image("player", "assets/shermie.png");
+        this.load.spritesheet("player", "assets/ShermieAnimation_v1.png", {
+            frameWidth: 64,
+            frameHeight: 64,
+            
+        });
         this.load.image("tiles", "assets/tilemap/ground_tileset.png");
         this.load.tilemapTiledJSON("map", "assets/tilemap/map_1.json");
     }
