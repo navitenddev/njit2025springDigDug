ca2a53ffd1db402f1d8f7175d903c1e7342a8b32
Haleynp <haleynpatel@gmail.com>
2025-03-09 21:08:23 -0400

    Added background to start screen and fixed indentation

--- scenes/StartMenu.js
+++ scenes/StartMenu.js
@@ -1,42 +1,46 @@
-// StartMenu.js
 export default class StartMenu extends Phaser.Scene {
     constructor() {
       super({ key: 'StartMenu' });
     }
   
     preload() {
-        // add bg image and assets for start
+        this.load.image("StartBG", "assets/StartBackground.jpeg");
     }
   
     create() {
-      // temp background
-      this.cameras.main.setBackgroundColor('#2d2d2d');
+        let bg = this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, "StartBG");
+        let scaleX = this.cameras.main.width / bg.width;
+        let scaleY = this.cameras.main.height / bg.height;
+        let scale = Math.max(scaleX, scaleY); 
+        bg.setScale(scale);
   
-      // Initial title text box
-      const titleText = this.add.text(
-        this.cameras.main.centerX,
-        this.cameras.main.centerY - 100,
-        'My Game',
-        { fontSize: '64px', fill: '#fff' }
-      );
-      titleText.setOrigin(0.5);
+        // Initial title text box
+        const titleText = this.add.text(
+            this.cameras.main.centerX,
+            this.cameras.main.centerY - 100,
+            'My Game',
+            { fontSize: '64px', fill: '#fff' }
+        );
+        titleText.setOrigin(0.5);
+    
+        // Initial instruction text
+        const startText = this.add.text(
+            this.cameras.main.centerX,
+            this.cameras.main.centerY,
+            'Press SPACE to Start',
+            { fontSize: '32px', fill: '#fff' }
+        );
+        startText.setOrigin(0.5);
+        titleText.setDepth(1);
+        startText.setDepth(1);
   
-      // Initial instruction text
-      const startText = this.add.text(
-        this.cameras.main.centerX,
-        this.cameras.main.centerY,
-        'Press SPACE to Start',
-        { fontSize: '32px', fill: '#fff' }
-      );
-      startText.setOrigin(0.5);
+        this.input.keyboard.on('keydown-SPACE', () => {
+            this.scene.start('PreloadScene');
+        });
   
-    this.input.keyboard.on('keydown-SPACE', () => {
-        this.scene.start('PreloadScene');
-      });
-  
-      this.input.on('pointerdown', () => {
-        this.scene.start('PreloadScene');
-      });
+        this.input.on('pointerdown', () => {
+            this.scene.start('PreloadScene');
+        });
     }
   }
   
