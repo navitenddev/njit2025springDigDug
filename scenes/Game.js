7acc59818fe2827356dfd35f98e9c1594982a4a3
Haleynp <haleynpatel@gmail.com>
2025-04-14 15:36:55 -0400

    add tilemaps for each level

--- scenes/Game.js
+++ scenes/Game.js
@@ -21,7 +21,7 @@ export default class GameScene extends Phaser.Scene {
         this.highScore = parseInt(localStorage.getItem("highScore")) || 0;
 
         // Create tilemap
-        this.map = this.make.tilemap({ key: "map" });
+        this.map = this.make.tilemap({ key: "map2" });
         const tileset = this.map.addTilesetImage("ground_tiles", "tiles");
         const groundLayer = this.map.createLayer("Ground", tileset, 0, 0);
 
