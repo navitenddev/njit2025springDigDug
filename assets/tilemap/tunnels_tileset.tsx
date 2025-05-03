196db8324cb82ce45350c04fdd96d33bdb45b5b9
fmartinez0530 <123339301+fmartinez0530@users.noreply.github.com>
2025-05-03 17:52:13 -0400

    Fix: enemies can reach the surface from the pre-dug out tunnel

--- assets/tilemap/tunnels_tileset.tsx
+++ assets/tilemap/tunnels_tileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tunnels_tileset" tilewidth="50" tileheight="50" tilecount="7" columns="7">
- <image source="tunnels_tileset.png" width="350" height="50"/>
+<tileset version="1.10" tiledversion="1.11.2" name="tunnels_tileset" tilewidth="50" tileheight="50" tilecount="8" columns="8">
+ <image source="tunnels_tileset.png" width="400" height="50"/>
  <tile id="0">
   <properties>
    <property name="up" type="int" value="6"/>
@@ -38,6 +38,13 @@
    <property name="down" value="6"/>
    <property name="left" type="int" value="6"/>
    <property name="right" type="int" value="6"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="left" type="int" value="6"/>
+   <property name="right" type="int" value="6"/>
+   <property name="up" type="int" value="6"/>
   </properties>
  </tile>
 </tileset>
