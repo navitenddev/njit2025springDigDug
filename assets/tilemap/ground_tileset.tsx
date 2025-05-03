9eeb5c2991d17cc327092db3b3acd86fdc9c343d
fmartinez0530 <123339301+fmartinez0530@users.noreply.github.com>
2025-05-03 12:09:25 -0400

    Update maps with new sprites

--- assets/tilemap/ground_tileset.tsx
+++ assets/tilemap/ground_tileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="ground_tileset" tilewidth="50" tileheight="50" tilecount="14" columns="7">
- <image source="ground_tileset.png" width="350" height="100"/>
+<tileset version="1.10" tiledversion="1.11.2" name="ground_tileset" tilewidth="50" tileheight="50" tilecount="35" columns="7">
+ <image source="ground_tileset.png" width="350" height="250"/>
  <tile id="0">
   <properties>
    <property name="down" type="int" value="0"/>
@@ -106,6 +106,134 @@
   </properties>
  </tile>
  <tile id="13">
+  <properties>
+   <property name="down" type="int" value="0"/>
+   <property name="left" type="int" value="0"/>
+   <property name="right" type="int" value="0"/>
+   <property name="up" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="down" type="int" value="0"/>
+   <property name="left" type="int" value="0"/>
+   <property name="right" type="int" value="0"/>
+   <property name="up" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="down" type="int" value="0"/>
+   <property name="left" type="int" value="0"/>
+   <property name="right" type="int" value="0"/>
+   <property name="up" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="down" type="int" value="0"/>
+   <property name="left" type="int" value="0"/>
+   <property name="right" type="int" value="0"/>
+   <property name="up" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="down" type="int" value="0"/>
+   <property name="left" type="int" value="0"/>
+   <property name="right" type="int" value="0"/>
+   <property name="up" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="down" type="int" value="0"/>
+   <property name="left" type="int" value="0"/>
+   <property name="right" type="int" value="0"/>
+   <property name="up" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="down" type="int" value="0"/>
+   <property name="left" type="int" value="0"/>
+   <property name="right" type="int" value="0"/>
+   <property name="up" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="down" type="int" value="0"/>
+   <property name="left" type="int" value="0"/>
+   <property name="right" type="int" value="0"/>
+   <property name="up" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="down" type="int" value="0"/>
+   <property name="left" type="int" value="0"/>
+   <property name="right" type="int" value="0"/>
+   <property name="up" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="down" type="int" value="0"/>
+   <property name="left" type="int" value="0"/>
+   <property name="right" type="int" value="0"/>
+   <property name="up" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="down" type="int" value="0"/>
+   <property name="left" type="int" value="0"/>
+   <property name="right" type="int" value="0"/>
+   <property name="up" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="down" type="int" value="0"/>
+   <property name="left" type="int" value="0"/>
+   <property name="right" type="int" value="0"/>
+   <property name="up" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="down" type="int" value="0"/>
+   <property name="left" type="int" value="0"/>
+   <property name="right" type="int" value="0"/>
+   <property name="up" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="down" type="int" value="0"/>
+   <property name="left" type="int" value="0"/>
+   <property name="right" type="int" value="0"/>
+   <property name="up" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="down" type="int" value="0"/>
+   <property name="left" type="int" value="0"/>
+   <property name="right" type="int" value="0"/>
+   <property name="up" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="down" type="int" value="0"/>
+   <property name="left" type="int" value="0"/>
+   <property name="right" type="int" value="0"/>
+   <property name="up" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="32">
   <properties>
    <property name="down" type="int" value="0"/>
    <property name="left" type="int" value="0"/>
